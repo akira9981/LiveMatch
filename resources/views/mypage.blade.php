90d0cab25469b77bb525c132a019ff180366f690
akira9981 <56640109+akira9981@users.noreply.github.com>
2020-09-26 20:25:30 +0900

    my page 作成

---
+++ resources/views/mypage.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>mypage</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MeetingController;
+use App\Http\Controllers\MyPageController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,5 +24,6 @@ use App\Http\Controllers\MeetingController;
 // })->name('dashboard');
 
 Route::get('/meeting', [MeetingController::class, 'index']);
+Route::get('/mypage', [MyPageController::class, 'index']);
 
 Route::get('/home', [HomeController::class, 'index']);
