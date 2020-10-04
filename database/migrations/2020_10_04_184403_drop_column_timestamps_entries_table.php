22c2028692d7592a2edd2b50f7e65213c784eba5
akira9981 <56640109+akira9981@users.noreply.github.com>
2020-10-05 03:46:56 +0900

    entries table created_at作成

---
+++ database/migrations/2020_10_04_184403_drop_column_timestamps_entries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropColumnTimestampsEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('entries', function (Blueprint $table) {
+            $table->dropColumn('created_at');
+            $table->dropColumn('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('entries', function (Blueprint $table) {
+            $table->timestamps();
+        });
+    }
+}
