b13ea52586328c3e483ccb4404004b93485ca99c
akira9981 <56640109+akira9981@users.noreply.github.com>
2020-10-24 21:53:53 +0900

    Update search.blade.php

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,13 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<p>検索結果</p>
+<p>検索結果</p><br>
+<p>meeting</p>
   @foreach ($meetings as $meeting)
-    <tr>
-        <td>{{ $meeting->title }}</td><br>
-        <td>{{ $meeting->capacity}}</td><br>
-        <td>{{ $meeting->detail }}</td><br>
-    </tr>
+    <td>{{ $meeting->title }}</td><br>
+    <td>{{ $meeting->detail }}</td><br>
+    <a href={{url("meeting/{$meeting->id}")}}>詳細へ進む<a>
     <p>-----------</p>
   @endforeach
 @endsection
