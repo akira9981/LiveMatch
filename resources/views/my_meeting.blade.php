8e5747be8297f2ce802eb3d7de556c830d640939
akira9981 <56640109+akira9981@users.noreply.github.com>
2020-11-08 22:10:09 +0900

    Update my_meeting.blade.php

--- resources/views/my_meeting.blade.php
+++ resources/views/my_meeting.blade.php
@@ -11,21 +11,24 @@
 @else
   @foreach($meetings as $meeting)
     <div class="card-body">
-        @if(!empty($meeting->user->image))
-        <div class='image-wrapper'><img class='review-image' src={{ $meeting->image }}></div>
-        @else
-        <div class='image-wrapper'><img class='review-image' src="{{ asset('images/noimage.jpg') }}"></div>
-        @endif
-        <h3 class='review-title'>{{ $meeting->title }}</h3>
-        <p class='description'>
-            {{ $meeting->capacity }}<br>
+      @if(!empty($meeting->user->image))
+      <div class='image-wrapper'><img class='image' src={{ $meeting->image }}></div>
+      @else
+      <div class='image-wrapper'><img class='image' src="{{ asset('images/noimage.jpg') }}"></div>
+      @endif
+
+      <div class='meeting-wrapper'>
+        <div class='meeting-title'>{{ $meeting->title }}</div>
+        <p class='meeting-detail'>
+            募集人数　{{ $meeting->capacity }}人<br>
             {{ $meeting->detail }}<br>
         </p>
         <div class="detail-btn">
-        <a href={{url("meeting/{$meeting->id}/edit")}}>編集する<a>
-        <a href={{url("meeting/{$meeting->id}/delete")}}>削除する<a>
-        <a href={{url("meeting/{$meeting->id}")}}>詳細へ進む<a>
+          <a href={{url("meeting/{$meeting->id}/edit")}}>編集する<a>
+          <a href={{url("meeting/{$meeting->id}/delete")}}>削除する<a>
+          <a href={{url("meeting/{$meeting->id}")}}>詳細へ進む<a>
         </div>
+      </div>
     </div>
   @endforeach
 @endif
