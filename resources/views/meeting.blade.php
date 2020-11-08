97f637291d9fb85bc1c19bbf125bd0cf2d28388b
akira9981 <56640109+akira9981@users.noreply.github.com>
2020-11-08 21:58:02 +0900

    meeting fix

--- resources/views/meeting.blade.php
+++ resources/views/meeting.blade.php
@@ -5,47 +5,50 @@
         </h2>
 </x-slot>
 <div class="main-contents">
-<div class="card-body">
-    @if(!empty($meeting->user->image))
-    <div class='image-wrapper'><img class='review-image' src={{ $meeting->image }}></div>
-    @else
-    <div class='image-wrapper'><img class='review-image' src="{{ asset('images/noimage.jpg') }}"></div>
-    @endif
-    <h3 class='review-title'></h3>
-    <p class='description'>
-        {{ $meeting->user->name }}
-        {{ $meeting->user->age }}
-        {{ $meeting->user->gender }}
-        {{ $meeting->user->profile }}
-    </p>
-
-    <div class="detail-btn">
-    @if($meeting->user->id == Auth::id())
-        <p></p>
-    @else
-        @if(empty($switching->user_id))
-        <a href={{url("meeting/{$meeting->id}/entry")}}>応募する<a>
+<div class="detail-wrapper">
+    <div class="card-body">
+        @if(!empty($meeting->user->image))
+        <div class='image-wrapper'><img class='image' src={{ $meeting->image }}></div>
         @else
-        <a href={{url("meeting/{$meeting->id}/cancel")}}>取り消す<a>
+        <div class='image-wrapper'><img class='image' src="{{ asset('images/noimage.jpg') }}"></div>
         @endif
-    @endif
-    <a href={{url("home")}}>ホームへ戻る<a>
+        <div class='description'>
+            <p>{{ $meeting->user->name }}</p>
+            <p>{{ $meeting->user->age }}歳</p>
+            <p>{{ $meeting->user->gender }}</p>
+            <p>{{ $meeting->user->profile }}</p>
+        </div>
+
+        <div class="detail-btn">
+        @if($meeting->user->id == Auth::id())
+            <p></p>
+        @else
+            @if(empty($switching->user_id))
+            <a href={{url("meeting/{$meeting->id}/entry")}}>応募する<a>
+            @else
+            <a href={{url("meeting/{$meeting->id}/cancel")}}>取り消す<a>
+            @endif
+        @endif
+        <a href={{url("home")}}>ホームへ戻る</a>
+        </div>
+    </div>
+    <div class="meeting-detail">
+        <p>{{$meeting->title}}</p>
+        <p>{{ $meeting->capacity }}</p>
+        <p>{{ $meeting->detail }}</p>
     </div>
 </div>
-{{$meeting->title}}
-{{ $meeting->capacity }}
-{{ $meeting->detail }}
-<p class>{{ $total }}　人から応募があります。</p>
-
-@foreach($entries as $entry)
-    <div class="card-body">
-        @if(!empty($entry->user->image))
-        <div class='image-wrapper'><img class='review-image' src={{ $entry->user->image }}></div>
-        @else
-        <div class='image-wrapper'><img class='review-image' src="{{ asset('images/noimage.jpg') }}"></div>
-        @endif
-        <h3 class='review-title'>{{ $entry->user->name }}</h3>
-    </div>
-@endforeach
+<div class="entry-user-wrapper">
+    <p class="title">{{ $total }}　人から応募があります。</p>
+    @foreach($entries as $entry)
+        <div class="entry-body">
+            @if(!empty($entry->user->image))
+            <div class='image-wrapper'><img class='image' src={{ $entry->user->image }}></div>
+            @else
+            <div class='image-wrapper'><img class='image' src="{{ asset('images/noimage.jpg') }}"></div>
+            @endif
+            <p class='entry-user'>{{ $entry->user->name }}</p>
+        </div>
+    @endforeach
 </div>
 </x-app-layout>
