62641035aba08576ea411a0b1a06f98024207e8f
akira9981 <56640109+akira9981@users.noreply.github.com>
2020-11-10 20:10:39 +0900

    create page

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,11 +5,9 @@
         </h2>
 </x-slot>
 <div class="main-contents">
-<div class="review-contents">
-  <h1 class='pagetitle'>投稿ページ</h1>
   <form method='post' action="/create">
     {{ csrf_field() }}
-    <div class="review-body">
+    <div class="post-body">
       <div class="form-group">
         <label>title</label>
         <input type='text' class='form-control' name='title' placeholder='タイトルを入力'>
@@ -20,13 +18,12 @@
       </div>
       <div class="form-group">
         <label>詳細</label>
-        <input type='text' class='form-control' name='detail' placeholder='詳細を入力'>
+        <textarea type='text' class='form-control' name='detail' placeholder='詳細を入力'></textarea>
       </div>
-      <div class="btn-wrapper">
+      <div class="create-btn">
         <input type='submit' class='btn-primary' value='募集する'>
       </div>
     </div>
   </form>
-</div>
 </div>
 </x-app-layout>
