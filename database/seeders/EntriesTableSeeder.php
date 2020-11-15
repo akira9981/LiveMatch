c2d763db83bfa363c65c523df0633dd2aa6067f5
akira9981 <56640109+akira9981@users.noreply.github.com>
2020-11-15 23:40:14 +0900

    seeder entries

--- database/seeders/EntriesTableSeeder.php
+++ database/seeders/EntriesTableSeeder.php
@@ -18,20 +18,54 @@ class EntriesTableSeeder extends Seeder
     {
       DB::table('entries')->insert([
         [
-            'user_id' => 5,
+            'user_id' => 1,
             'meetings_id' => 2,
+            'created_at' => '2020-10-19 21:26:35'
         ],
         [
-            'user_id' => 6,
-            'meetings_id' => 1,
+            'user_id' => 2,
+            'meetings_id' => 3,
+            'created_at' => '2020-10-19 21:26:35'
+        ],
+        [
+            'user_id' => 3,
+            'meetings_id' => 4,
+            'created_at' => '2020-10-19 21:26:35'
+        ],
+        [
+            'user_id' => 4,
+            'meetings_id' => 5,
+            'created_at' => '2020-10-19 21:26:35'
         ],
         [
             'user_id' => 5,
-            'meetings_id' => 3,
+            'meetings_id' => 1,
+            'created_at' => '2020-10-19 21:26:35'
         ],
         [
-            'user_id' => 7,
-            'meetings_id' => 1,
+            'user_id' => 1,
+            'meetings_id' => 6,
+            'created_at' => '2020-10-19 21:26:35'
+        ],
+        [
+            'user_id' => 1,
+            'meetings_id' => 7,
+            'created_at' => '2020-10-19 21:26:35'
+        ],
+        [
+            'user_id' => 1,
+            'meetings_id' => 8,
+            'created_at' => '2020-10-19 21:26:35'
+        ],
+        [
+            'user_id' => 1,
+            'meetings_id' => 9,
+            'created_at' => '2020-10-19 21:26:35'
+        ],
+        [
+            'user_id' => 2,
+            'meetings_id' => 10,
+            'created_at' => '2020-10-19 21:26:35'
         ],
       ]);
     }
