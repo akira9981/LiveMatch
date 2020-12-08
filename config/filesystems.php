1154855139e2733050198aa0b11bf03a90cfc1ec
akira9981 <56640109+akira9981@users.noreply.github.com>
2020-12-08 11:26:37 +0900

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,10 +57,10 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
+            'key' => env('AWS_ACCESS_KEY_ID','AKIA2AIGYKMW2CN5X7XK'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY','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'),
+            'region' => env('AWS_DEFAULT_REGION','ap-northeast-1'),
+            'bucket' => env('AWS_BUCKET','live-match'),
         ],
 
     ],
