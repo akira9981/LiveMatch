37e8d78f116d1d36c5fa79e0bbed7e20cbc157e4
akira9981 <56640109+akira9981@users.noreply.github.com>
2020-12-12 17:58:39 +0900

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     {
         View::share('header' , '');
         View::share('slot' , '');
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
